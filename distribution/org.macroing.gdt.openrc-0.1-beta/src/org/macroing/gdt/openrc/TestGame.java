3a00a1a2946826afb5d7ed0e0dc7ba3da8d61d51
WavePropagation <joergen_lundgren@hotmail.com>
2015-08-17 22:20:49 +0200

    Fix: Some unused imports were removed.

--- distribution/org.macroing.gdt.openrc-0.1-beta/src/org/macroing/gdt/openrc/TestGame.java
+++ distribution/org.macroing.gdt.openrc-0.1-beta/src/org/macroing/gdt/openrc/TestGame.java
@@ -19,14 +19,7 @@
 package org.macroing.gdt.openrc;
 
 import java.awt.event.KeyEvent;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 
